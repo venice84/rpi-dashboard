3e4b89ebece665cd1063ba61910fa2b881e718e8
venice84 <63596604+venice84@users.noreply.github.com>
2020-04-13 21:23:25 +0200

    Update start_sensoriraspberry.sh

--- start_sensoriraspberry.sh
+++ start_sensoriraspberry.sh
@@ -4,7 +4,7 @@ uptime=$(uptime | awk '{print int($3+$5/1440),"giorni"}')
 disco_occ=$(df -h | grep /dev/root | awk '{print $5}')
 ram_occ=$(free -m | awk 'FNR == 2 {print int($3/$2*100)}')
 
-mosquitto_pub -h 192.168.10.114 -m "$raspcpu" -t raspcpu -u 'user' -P 'password' -r
-mosquitto_pub -h 192.168.10.114 -m "$uptime" -t uptime -u 'user' -P 'password' -r
-mosquitto_pub -h 192.168.10.114 -m "$disco_occ" -t disco_occ -u 'user' -P 'password' -r
-mosquitto_pub -h 192.168.10.114 -m "$ram_occ" -t ram_occ -u 'user' -P 'password' -r
+mosquitto_pub -h 192.168.x.x -m "$raspcpu" -t raspcpu -u 'user' -P 'password' -r
+mosquitto_pub -h 192.168.x.x -m "$uptime" -t uptime -u 'user' -P 'password' -r
+mosquitto_pub -h 192.168.x.x -m "$disco_occ" -t disco_occ -u 'user' -P 'password' -r
+mosquitto_pub -h 192.168.x.x -m "$ram_occ" -t ram_occ -u 'user' -P 'password' -r
